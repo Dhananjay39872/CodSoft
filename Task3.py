4abe04d9ff0f52c3182957679245c9fa25423312
DHANANJAY PRAJAPATI <dp751ji@gmail.com>
2026-04-22 13:46:20 +0530

    Added password generator task3

---
+++ Task3.py
@@ -0,0 +1,14 @@
+# password generator task3
+import random
+import string
+
+length = int(input("enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
